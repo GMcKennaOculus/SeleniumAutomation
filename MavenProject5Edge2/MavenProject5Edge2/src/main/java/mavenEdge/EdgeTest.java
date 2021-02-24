baae839acdbc9a0fe1215c01829c2c16a958dff7
GMcKennaOculus <79461528+GMcKennaOculus@users.noreply.github.com>
2021-02-24 14:15:54 -0500

    Add files via upload

---
+++ MavenProject5Edge2/MavenProject5Edge2/src/main/java/mavenEdge/EdgeTest.java
@@ -0,0 +1,26 @@
+package mavenEdge;
+
+import org.openqa.selenium.edge.EdgeDriver;
+
+//import org.openqa.selenium.edge.EdgeDriver;
+
+public class EdgeTest {
+
+	public EdgeTest() {
+	System.setProperty("webdriver.edge.driver","C:/Users/gmcke/oculus/automation/Drivers/Edge/edgedriver_win64/msedgedriver.exe");
+	
+	
+	EdgeDriver ed = new EdgeDriver();
+	try {
+		ed.wait(1000);
+	}catch(Exception e){
+	}
+	ed.get("https://www.cnn.com");
+	try {
+		Thread.sleep(1000);
+	}catch(Exception e){
+	}
+	ed.close();
+	}
+	
+	}
---
+++ MavenProject5Edge2/MavenProject5Edge2/src/main/java/mavenEdge/Main.java
@@ -0,0 +1,11 @@
+package mavenEdge;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EdgeTest et = new EdgeTest();
+		
+	}
+
+}
